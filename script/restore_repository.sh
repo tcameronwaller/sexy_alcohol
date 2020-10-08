d43e2bc46700434c6839e0529e29b254415fe9d9
T. Cameron Waller <tcameronwaller@gmail.com>
2020-10-08 15:15:00 -0500

    added path to temporary directory

--- script/restore_repository.sh
+++ script/restore_repository.sh
@@ -4,23 +4,25 @@
 
 # Read in private file paths.
 echo "read private file path variables..."
-path_path="~/path"
-path_repository=$(<"$path_path/sexy_alcohol.txt")
+cd ~/path
+path_repository=$(<"./sexy_alcohol.txt")
+path_temporary=$(<"./process_temporary.txt")
 echo $path_repository
+echo $path_temporary
 
 # Echo each command to console.
 #set -x
 
 # Remove previous version of program.
 
-#echo "remove previous version of the repository..."
-
+echo "remove previous version of the repository..."
 #rm -r $path_repository
 
 # Access current version of the program.
 
-#echo "access current version of the program..."
-
+echo "access current version of the program..."
+cd $path_temporary
 #wget https://github.com/tcameronwaller/sexy_alcohol/archive/main.zip
-#mv main.zip sexy_alcohol.zip
 #unzip sexy_alcohol.zip
+#mv sexy_alcohol-main $path_repository
+#rm main.zip
