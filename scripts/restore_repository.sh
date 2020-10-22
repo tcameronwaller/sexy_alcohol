968c6d2ad005792534c8d15355d55025eb8fdd5c
T. Cameron Waller <tcameronwaller@gmail.com>
2020-10-22 10:53:23 -0500

    organized promiscuity as actual subpackage

--- scripts/restore_repository.sh
+++ scripts/restore_repository.sh
@@ -4,20 +4,20 @@
 
 # Read in private file paths.
 echo "read private file path variables..."
-cd ~/path
+cd ~/paths
 path_temporary=$(<"./temporary_sexy_alcohol.txt")
 path_waller="$path_temporary/waller"
-path_repository="$path_waller/sexy_alcohol"
-path_package="$path_repository/package"
-path_subpackage="$path_package/promiscuity"
+path_sexy_alcohol="$path_waller/sexy_alcohol"
+path_promiscuity="$path_waller/promiscuity"
 
 # Echo each command to console.
 set -x
 
 # Remove previous version of program.
 
-echo "remove previous version of the repository..."
-rm -r $path_repository
+echo "remove previous versions of the repositories..."
+rm -r $path_sexy_alcohol
+rm -r $path_promiscuity
 
 # Access current version of the program.
 
@@ -26,12 +26,15 @@ cd $path_waller
 wget https://github.com/tcameronwaller/sexy_alcohol/archive/main.zip
 unzip main.zip
 rm main.zip
-mv sexy_alcohol-main $path_repository
+mv sexy_alcohol-main $path_sexy_alcohol
 
 echo "access current version of the promiscuity repository..."
-echo "organize promiscuity as a subpackage within sexy_alcohol..."
-cd $path_package
+cd $path_waller
 wget https://github.com/tcameronwaller/promiscuity/archive/main.zip
 unzip main.zip
 rm main.zip
-mv promiscuity-main $path_subpackage
+mv promiscuity-main $path_promiscuity
+
+echo "organize promiscuity as a subpackage within sexy_alcohol..."
+cp -r "$path_promiscuity/package" "$path_sexy_alcohol/package/package"
+mv "$path_sexy_alcohol/package/package" "$path_sexy_alcohol/package/promiscuity"
