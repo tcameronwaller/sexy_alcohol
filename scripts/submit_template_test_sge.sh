99fc2899ca43dedbef96dd5f71fa2a003788efa0
T. Cameron Waller <tcameronwaller@gmail.com>
2020-10-22 19:13:24 -0500

    set up test run with SGE

--- scripts/submit_template_test_sge.sh
+++ scripts/submit_template_test_sge.sh
@@ -28,6 +28,8 @@ cd ~/paths
 path_temporary=$(<"./temporary_sexy_alcohol.txt")
 path_waller="$path_temporary/waller"
 path_play="$path_waller/play_with_sge"
+path_source="$path_play/source"
+path_product="$path_play/product"
 path_sexy_alcohol="$path_waller/sexy_alcohol"
 path_scripts="$path_sexy_alcohol/scripts"
 
@@ -37,32 +39,42 @@ path_scripts="$path_sexy_alcohol/scripts"
 set +x
 
 # Determine whether the temporary directory structure already exists.
-if [ ! -d $path_play ]
-then
+if [ ! -d $path_play ]; then
     # Directory does not already exist.
     # Create directory.
     mkdir -p $path_play
+    mkdir -p $path_source
+    mkdir -p $path_product
 fi
 
 # Organize instances for iteration.
-echo "becky marissa amanda avianna bethany" | tr -s " " "\n" > "$path_play/names.txt"
-readarray -t names < "$path_play/names.txt"
+echo "----------------------------------------------------------------------"
+echo "Demonstrate a simple array."
+echo "----------------------------------------------------------------------"
+echo "becky marissa amanda avianna bethany" | tr -s " " "\n" > "$path_source/names.txt"
+readarray -t names < "$path_source/names.txt"
 echo ${names[@]}
 count=${#names[@]}
-indices_maximum=$((count-1))
+index_maximum=$((count-1))
 echo "count of names: "
 echo $count
 echo ${indices[@]}
 
 # Iterate on instances.
+echo "----------------------------------------------------------------------"
+echo "Iterate on that simple array with indices."
+echo "----------------------------------------------------------------------"
 #for name in "${names[@]}"; do
 #for name in "${names[@]:0:100}"; do
-for (( index=0; index<=$indices_maximum; index+=1 )); do
+for (( index=0; index<=$index_maximum; index+=1 )); do
     name=${names[$index]}
     echo "current name: "
     echo $name
 done
 
 # Execute procedure(s).
-
-#qsub $path_scripts/template_test_sge.sh $path_play
+echo "----------------------------------------------------------------------"
+echo "Submit array batch to Sun Grid Engine."
+echo "----------------------------------------------------------------------"
+qsub -t 1-[$index_maximum]:1 -o "$path_product/out.txt" -e "$path_product/error.txt" \
+$path_scripts/template_test_sge.sh $path_source $path_product $count
--- scripts/template_test_sge.sh
+++ scripts/template_test_sge.sh
@@ -1,1 +1,69 @@
 #!/bin/bash
+
+###########################################################################
+# Specify arguments for qsub command.
+# Note that bash does not interpret qsub parameters, which are bash comments.
+# Bash will not expand variables in qsub parameters.
+# Shell.
+#$ -S /bin/bash
+# Name of job.
+#$ -N waller_test
+# Contact.
+#$ -M waller.tcameron@mayo.edu
+#$ -m abes
+# Standard output and error.
+# Specify as arguments when calling qsub.
+### -o "./out"
+### -e "./error"
+# Queue.
+# "1-hour", "1-day", "4-day", "7-day", "30-day", "lg-mem"
+#$ -q 1-hour
+# Priority 0-15.
+### -p -10
+# Memory per iteration.
+# Segmentation errors commonly indicate a memory error.
+#$ -l h_vmem=50G
+# Concurrent threads; assigns value to variable NSLOTS.
+#$ -pe threaded 16
+# Range of indices.
+# Specify as argument when calling qsub.
+### -t 1-100:1
+# Limit on concurrent processes.
+#$ -tc 10
+
+###########################################################################
+###########################################################################
+###########################################################################
+# This script organizes an array batch job for the Sun Grid Engine.
+# mForge
+# 120 worker nodes, each with 32 CPUs and 512 Gigabytes memory
+# 3 worker nodes, each with 64 CPUs and 1.5 Terabytes memory
+###########################################################################
+###########################################################################
+###########################################################################
+
+
+###########################################################################
+# Organize variables.
+index=$SGE_TASK_ID
+path_source=$1
+path_product=$2
+count=$3
+
+###########################################################################
+# Execute procedure.
+
+# Read source.
+readarray -t names < "$path_source/names.txt"
+name=${names[$index]}
+
+# Create directory and file.
+mkdir -p "$path_product/$name"
+echo $name > "$path_product/$name/$name.txt"
+
+# Report.
+echo "----------"
+echo "task index: " $index
+echo "name: " $name
+hostname
+date
