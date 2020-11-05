1170eb884d10525014297106b56ebfa791ded446
T. Cameron Waller <tcameronwaller@gmail.com>
2020-11-05 16:56:57 -0600

    version check

--- package/assembly.py
+++ package/assembly.py
@@ -894,7 +894,7 @@ def execute_procedure(
 
     utility.print_terminal_partition(level=1)
     print(path_dock)
-    print("version check: 3")
+    print("version check: 5")
 
     # Read source information from file.
     # Exclusion identifiers are "eid".
