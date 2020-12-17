47371895b65e68dbbae2c7383833526806b424a2
T. Cameron Waller <tcameronwaller@gmail.com>
2020-12-16 20:13:05 -0600

    ready for next batch

--- scripts/gwas/2020-12-17_cohorts/submit_gwas_alcoholism_cohorts.sh
+++ scripts/gwas/2020-12-17_cohorts/submit_gwas_alcoholism_cohorts.sh
@@ -17,7 +17,7 @@ echo "----------------------------------------------------------------------"
 echo "----------"
 echo "The script organizes and submits an array batch job."
 echo "----------"
-echo "version check: 2"
+echo "version check: 3"
 echo "----------------------------------------------------------------------"
 echo "----------------------------------------------------------------------"
 echo "----------------------------------------------------------------------"
@@ -32,8 +32,8 @@ path_dock="$path_temporary/waller/dock"
 path_scripts="$path_waller/sexy_alcohol/scripts/gwas"
 
 # female_alcoholism_1_oestradiol: job 1943204, job 1943205
-# female_alcoholism_1_testosterone:
 # male_alcoholism_1_oestradiol: job 1943206, job 1943207
+# female_alcoholism_1_testosterone: job 1943208, job 1943209
 # male_alcoholism_1_testosterone:
 
 # Case-specific parameters.
@@ -41,7 +41,7 @@ threads=16
 maf=0.01
 count=22 # 22 # Count of chromosomes on which to run GWAS
 covariates="age,body_mass_index,genotype_pc_1,genotype_pc_2,genotype_pc_3,genotype_pc_4,genotype_pc_5,genotype_pc_6,genotype_pc_7,genotype_pc_8,genotype_pc_9,genotype_pc_10"
-sex="female"
+sex="male"
 alcoholism="alcoholism_1"
 hormone="testosterone"
 analysis="${sex}_${alcoholism}_${hormone}"
