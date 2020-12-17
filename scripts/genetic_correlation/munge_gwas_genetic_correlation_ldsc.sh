9252355e70438e8452419a367c56e325c4d71ef0
T. Cameron Waller <tcameronwaller@gmail.com>
2020-12-17 12:01:48 -0600

    multiple scripts to organize GWAS summary statistics and execute LDSC genetic correlation

---
+++ scripts/genetic_correlation/munge_gwas_genetic_correlation_ldsc.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+# ...
+###########################################################################
+###########################################################################
+###########################################################################
+
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+echo "Munge GWAS summary statistics for LDSC."
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+echo ""
+echo ""
+echo ""
+
+# Organize variables.
+path_gwas_one=$1 # path to GWAS report files for phenotype one
+path_gwas_two=$2 # path to GWAS report files for phenotype two
+path_munge=$3 # path for munge GWAS report files
+path_report=$4 # path to directory for genetic correlation report
+phenotype_one=$5 # name of phenotype one
+phenotype_two=$6 # name of phenotype two
+path_alleles=$7 # path to reference for alleles
+path_disequilibrium=$8 # path to reference for linkage disequilibrium
+path_ldsc=$9 # path to LDSC
+
+###########################################################################
+# Munge GWAS summary statistics for LDSC.
+
+path_munge_one="$path_munge/${phenotype_one}"
+$path_ldsc/munge_sumstats.py \
+--sumstats $path_gwas_one \
+--out $path_munge_one \
+--merge-alleles $path_alleles/w_hm3.snplist
+
+path_munge_two="$path_munge/${phenotype_two}"
+$path_ldsc/munge_sumstats.py \
+--sumstats $path_gwas_two \
+--out $path_munge_two \
+--merge-alleles $path_alleles/w_hm3.snplist
+
+###########################################################################
+# Estimate genetic correlation in LDSC.
+
+path_munge_one_complete="$path_munge/${phenotype_one}.sumstats.gz"
+path_munge_two_complete="$path_munge/${phenotype_two}.sumstats.gz"
+
+$path_ldsc/ldsc.py \
+--rg $path_munge_one_complete,$path_munte_two_complete \
+--ref-ld-chr $path_disequilibrium/eur_w_ld_chr/ \
+--w-ld-chr $path_disequilibrium/eur_w_ld_chr/ \
+--out $path_report
---
+++ scripts/gwas/concatenate_organize_gwas_ldsc.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+# ...
+###########################################################################
+###########################################################################
+###########################################################################
+
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+echo "Concatenate GWAS summary statistics across chromosomes."
+echo "version 1"
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+echo ""
+echo ""
+echo ""
+
+# Organize variables.
+path_gwas=$1 # path to directory with GWAS report files
+prefix=$2 # prefix for GWAS report files
+phenotype=$3 # name of phenotype in names of GWAS report files
+chromosomes=$4 # count of chromosomes
+
+# Concatenate GWAS reports from all chromosomes.
+# Extract relevant information and format for LDSC.
+# Initialize concatenation.
+cd $path_gwas
+path_concatenation="$path_gwas/concatenation.${phenotype}.glm.linear"
+
+rm $path_concatenation
+
+echo "SNP A1 A2 N BETA P" > $path_concatenation
+for (( index=1; index<=$chromosomes; index+=1 )); do
+  path_gwas_chromosome="$path_gwas/chromosome_${index}"
+  echo "gwas chromosome path: "
+  echo $path_gwas_chromosome
+  path_report="$path_gwas_chromosome/${prefix}.${phenotype}.glm.linear"
+  # Select and concatenate relevant information from chromosome reports.
+  # Format of GWAS reports (".glm.linear") by PLINK2.
+  # https://www.cog-genomics.org/plink/2.0/formats
+  # Format of GWAS summary for LDSC.
+  # https://github.com/bulik/ldsc/wiki/Heritability-and-Genetic-Correlation#reformatting-summary-statistics
+  # description: ............................ PLINK2 column ... LDSC column
+  # variant identifier: ..................... "ID" ............ "SNP"
+  # alternate allele (effect allele): ....... "A1" ............ "A1"
+  # reference allele (non-effect allele): ... "REF" ........... "A2"
+  # sample size: ............................ "OBS_CT" ........ "N"
+  # effect (beta): .......................... "BETA" .......... "BETA"
+  # probability (p-value): .................. "P" ............. "P"
+  cat $path_report | awk 'NR > 1 {print $3, $6, $4, $8, $9, $12}' >> $path_concatenation
+done
+echo "----------"
+echo "----------"
+echo "----------"
+echo "after concatenation..."
+head -30 $path_concatenation
