e45e5545a2b63a8108378a2dd7b34ad2b0aa596f
T. Cameron Waller <tcameronwaller@gmail.com>
2020-12-18 08:03:59 -0600

    manage argument references

--- scripts/record/2020-12-18/6_regress_genetic_correlation_sex_hormone.sh
+++ scripts/record/2020-12-18/6_regress_genetic_correlation_sex_hormone.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Organize variables.
-sex=$1 # name of sex
-alcoholism=$2 # name of definition of alcoholism
-hormone=$3 # name of hormone
-path_gwas=$4 # path to directory for GWAS summary statistics
-path_genetic_correlation=$5 # path to directory for genetic correlations
-path_gwas_scripts=$6 # path to directory with utility scripts for GWAS
-path_correlation_scripts=$7 # path to directory with utility scripts for genetic correlation
-path_ldsc=$8 # path to LDSC
-path_alleles=$9 # path to reference for alleles
-path_disequilibrium=$10 # path to reference for linkage disequilibrium
+sex=${1} # name of sex
+alcoholism=${2} # name of definition of alcoholism
+hormone=${3} # name of hormone
+path_gwas=${4} # path to directory for GWAS summary statistics
+path_genetic_correlation=${5} # path to directory for genetic correlations
+path_gwas_scripts=${6} # path to directory with utility scripts for GWAS
+path_correlation_scripts=${7} # path to directory with utility scripts for genetic correlation
+path_ldsc=${8} # path to LDSC
+path_alleles=${9} # path to reference for alleles
+path_disequilibrium=${10} # path to reference for linkage disequilibrium
 
 cohort_comparison="${sex}_${alcoholism}_${hormone}"
 
