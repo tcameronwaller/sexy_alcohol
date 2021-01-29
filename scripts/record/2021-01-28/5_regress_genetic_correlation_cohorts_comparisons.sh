e2796f486c4d8b35d0cfec87d8f3353d7fa762c6
T. Cameron Waller <tcameronwaller@gmail.com>
2021-01-28 19:19:09 -0600

    ready for male alcoholism_2 testosterone

--- scripts/record/2021-01-28/5_regress_genetic_correlation_cohorts_comparisons.sh
+++ scripts/record/2021-01-28/5_regress_genetic_correlation_cohorts_comparisons.sh
@@ -63,7 +63,7 @@ set +x
 
 if true; then
   # Parameters.
-  cohort_comparison="female_alcoholism-2_testosterone"
+  cohort_comparison="male_alcoholism-2_testosterone"
   alcoholism="alcoholism_2"
   hormone="testosterone"
   /usr/bin/bash "$path_correlation_scripts/6_regress_genetic_correlation_sex_hormone.sh" \
@@ -85,6 +85,24 @@ if false; then
   # Parameters.
   cohort_comparison="female_alcoholism-1_testosterone"
   alcoholism="alcoholism_1"
+  hormone="testosterone"
+  /usr/bin/bash "$path_correlation_scripts/6_regress_genetic_correlation_sex_hormone.sh" \
+  $cohort_comparison \
+  $alcoholism \
+  $hormone \
+  "logistic" \
+  "linear" \
+  $path_gwas \
+  $path_genetic_correlation \
+  $path_gwas_scripts \
+  $path_correlation_scripts \
+  $path_ldsc \
+  $path_alleles \
+  $path_disequilibrium
+
+  # Parameters.
+  cohort_comparison="female_alcoholism-2_testosterone"
+  alcoholism="alcoholism_2"
   hormone="testosterone"
   /usr/bin/bash "$path_correlation_scripts/6_regress_genetic_correlation_sex_hormone.sh" \
   $cohort_comparison \
