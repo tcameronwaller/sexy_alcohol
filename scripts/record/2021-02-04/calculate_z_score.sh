39b4bd4b684b29abbcd9fcec3e190e74f1a8c561
T. Cameron Waller <tcameronwaller@gmail.com>
2021-02-04 18:31:25 -0600

    try standard deviation formula from sum of squares

--- scripts/record/2021-02-04/calculate_z_score.sh
+++ scripts/record/2021-02-04/calculate_z_score.sh
@@ -14,8 +14,8 @@ mean=$(cat $path_table_original | awk -v column=$column 'BEGIN { FS=" " } NR > 1
 variations=$(cat $path_table_original | awk -v column="$column" -v mean="$mean" \
 'BEGIN { FS=" " } NR > 1 { value += (($column - mean)^2) } END { print value }')
 # The following formulas for standard deviation are equivalent.
-#standard_deviation=$(bc -l <<< "sqrt(($sum_squares - (($sum^2)/$count))/($count - 1))")
-standard_deviation=$(bc -l <<< "sqrt(($variations)/($count - 1))")
+standard_deviation=$(bc -l <<< "sqrt(($sum_squares - (($sum^2)/$count))/($count - 1))")
+#standard_deviation=$(bc -l <<< "sqrt(($variations)/($count - 1))")
 
 # Report statistics.
 echo "----------"
