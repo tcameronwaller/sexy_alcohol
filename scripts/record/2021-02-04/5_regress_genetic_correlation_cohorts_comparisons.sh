08e53aec912d61aaa09b6b454f41771c83f45293
T. Cameron Waller <tcameronwaller@gmail.com>
2021-02-05 08:52:06 -0600

    female alcoholism_1 steroid_globulin

--- scripts/record/2021-02-04/5_regress_genetic_correlation_cohorts_comparisons.sh
+++ scripts/record/2021-02-04/5_regress_genetic_correlation_cohorts_comparisons.sh
@@ -70,9 +70,9 @@ set +x
 if true; then
 
   # Parameters.
-  cohort_comparison="male_alcoholism-1_oestradiol"
+  cohort_comparison="female_alcoholism-1_steroid-globulin"
   alcoholism="alcoholism_1"
-  hormone="oestradiol"
+  hormone="steroid_globulin"
   /usr/bin/bash "$path_correlation_scripts/6_regress_genetic_correlation_sex_hormone.sh" \
   $cohort_comparison \
   $alcoholism \
