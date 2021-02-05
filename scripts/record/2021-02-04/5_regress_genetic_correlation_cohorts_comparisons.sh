0f7cb9a1a8f1c94593d63e5689a79f77b5d30869
T. Cameron Waller <tcameronwaller@gmail.com>
2021-02-05 09:12:51 -0600

    male alcoholism_1 albumin

--- scripts/record/2021-02-04/5_regress_genetic_correlation_cohorts_comparisons.sh
+++ scripts/record/2021-02-04/5_regress_genetic_correlation_cohorts_comparisons.sh
@@ -70,7 +70,7 @@ set +x
 if true; then
 
   # Parameters.
-  cohort_comparison="female_alcoholism-1_albumin"
+  cohort_comparison="male_alcoholism-1_albumin"
   alcoholism="alcoholism_1"
   hormone="albumin"
   /usr/bin/bash "$path_correlation_scripts/6_regress_genetic_correlation_sex_hormone.sh" \
