e02475b863552a0c0a6e2d0f32eaaba9de152257
T. Cameron Waller <tcameronwaller@gmail.com>
2021-05-05 20:06:50 -0500

    version check

---
+++ scripts/record/2021-05-07/6_format_munge_gwas_heritability_ldsc_cohorts_hormones.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+# ...
+###########################################################################
+###########################################################################
+###########################################################################
+
+################################################################################
+# Organize paths.
+# Read private, local file paths.
+cd ~/paths
+path_ldsc=$(<"./tools_ldsc.txt")
+path_gwas_summaries=$(<"./gwas_summaries_waller_metabolism.txt")
+path_process=$(<"./process_sexy_alcohol.txt")
+
+path_promiscuity_scripts="${path_process}/promiscuity/scripts"
+path_promiscuity_scripts_ldsc_heritability="${path_promiscuity_scripts}/ldsc_genetic_heritability_correlation"
+path_scripts_format="${path_promiscuity_scripts}/format_gwas_ldsc"
+#path_scripts_record="$path_process/psychiatric_metabolism/scripts/record/2021-05-04"
+
+path_dock="$path_process/dock"
+path_genetic_reference="${path_dock}/access/genetic_reference"
+path_gwas="${path_dock}/gwas"
+path_heritability="${path_dock}/heritability"
+
+###########################################################################
+# Execute procedure.
+
+#find . -maxdepth 1 -mindepth 1 -type d
