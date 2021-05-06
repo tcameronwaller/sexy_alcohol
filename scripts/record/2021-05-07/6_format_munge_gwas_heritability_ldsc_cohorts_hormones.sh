618bd272c4e125fbaff33614ce89a907c3673d88
T. Cameron Waller <tcameronwaller@gmail.com>
2021-05-06 10:30:06 -0500

    begin concatenation

--- scripts/record/2021-05-07/6_format_munge_gwas_heritability_ldsc_cohorts_hormones.sh
+++ scripts/record/2021-05-07/6_format_munge_gwas_heritability_ldsc_cohorts_hormones.sh
@@ -31,15 +31,46 @@ path_gwas_cohorts_hormones="${path_gwas}/cohorts_hormones"
 ###########################################################################
 # Execute procedure.
 
-# TODO: iterate on all cohort-hormone subdirectories
-#find . -maxdepth 1 -mindepth 1 -type d
+# TODO: for each cohort and hormone...
+# 1. keep the name of the cohort-hormone
+# 2. concatenate GWAS sum stats across chromosomes
+# 3. format GWAS sum stats
+# 4. munge GWAS sum stats...
+# 5. determine heritability
 
-#
+# maybe use the general script for steps 3, 4, and 5???
+
+# Iterate on directories for GWAS on cohorts and hormones.
 cd $path_gwas_cohorts_hormones
 for path_directory in `find . -maxdepth 1 -mindepth 1 -type d -not -name .`; do
   if [ -d "$path_directory" ]; then
     # Current content item is a directory.
     directory="$(basename -- $path_directory)"
     echo $directory
+
+    #study="${directory}"
+
+    # Concatenate GWAS across chromosomes.
+    # Organize variables.
+    pattern_source_file="report.*.glm.linear" # do not expand with full path yet
+    path_source_directory="${path_gwas_cohorts_hormones}/${directory}"
+    chromosome_start=1
+    chromosome_end=22
+    path_gwas_concatentation="${path_source_directory}/gwas_concatenation.txt"
+    path_gwas_concatentation_compress="${path_source_directory}/gwas_concatenation.txt.gz"
+    report="true" # "true" or "false"
+    /usr/bin/bash "${path_promiscuity_scripts}/collect_concatenate_gwas_chromosomes.sh" \
+    $pattern_source_file \
+    $path_source_directory \
+    $chromosome_start \
+    $chromosome_end \
+    $path_gwas_concatenation \
+    $path_gwas_concatenation_compress \
+    $report
+
   fi
 done
+
+
+# TODO: need a new general script
+# TODO: collect_concatenate_gwas_chromosomes.sh
