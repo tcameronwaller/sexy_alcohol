186d80ec301aff554671708168b59f46f657cc80
T. Cameron Waller <tcameronwaller@gmail.com>
2021-05-06 12:58:43 -0500

    include format, munge, and heritability

--- scripts/record/2021-05-07/6_format_munge_gwas_heritability_ldsc_cohorts_hormones.sh
+++ scripts/record/2021-05-07/6_format_munge_gwas_heritability_ldsc_cohorts_hormones.sh
@@ -49,15 +49,15 @@ for path_directory in `find . -maxdepth 1 -mindepth 1 -type d -not -name .`; do
     echo $directory
 
     # Concatenate GWAS across chromosomes.
-    # Organize variables.
-    pattern_source_file="report.*.glm.linear" # do not expand with full path yet
-    path_source_directory="${path_gwas_cohorts_hormones}/${directory}"
-    chromosome_start=1
-    chromosome_end=22
-    path_gwas_concatenation="${path_source_directory}/gwas_concatenation.txt"
-    path_gwas_concatenation_compress="${path_source_directory}/gwas_concatenation.txt.gz"
-    report="true" # "true" or "false"
-    if true; then
+    if false; then
+      # Organize variables.
+      pattern_source_file="report.*.glm.linear" # do not expand with full path yet
+      path_source_directory="${path_gwas_cohorts_hormones}/${directory}"
+      chromosome_start=1
+      chromosome_end=22
+      path_gwas_concatenation="${path_source_directory}/gwas_concatenation.txt"
+      path_gwas_concatenation_compress="${path_source_directory}/gwas_concatenation.txt.gz"
+      report="true" # "true" or "false"
       /usr/bin/bash "${path_promiscuity_scripts}/collect_concatenate_gwas_chromosomes.sh" \
       $pattern_source_file \
       $path_source_directory \
@@ -68,9 +68,32 @@ for path_directory in `find . -maxdepth 1 -mindepth 1 -type d -not -name .`; do
       $report
     fi
 
+    # Format and munge GWAS summary statistics.
+    # Estimate genotype heritability.
+    if true; then
+      # Organize paths.
+      path_gwas_study="${path_gwas_cohorts_hormones}/${directory}"
+      path_heritability_study="${path_heritability}/cohorts_hormones/${directory}"
+      # Initialize directories.
+      mkdir -p $path_gwas_study
+      mkdir -p $path_heritability_study
+      # Organize variables.
+      study="${directory}"
+      name_prefix="null"
+      path_source_file="${path_gwas_study}/gwas_concatenation.txt.gz"
+      path_script_gwas_format="${path_scripts_format}/format_gwas_ldsc_plink_linear.sh"
+      report="true" # "true" or "false"
+      /usr/bin/bash "$path_promiscuity_scripts_ldsc_heritability/format_munge_gwas_heritability_ldsc.sh" \
+      $study \
+      $name_prefix \
+      $path_source_file \
+      $path_genetic_reference \
+      $path_gwas_study \
+      $path_heritability_study \
+      $path_script_gwas_format \
+      $path_promiscuity_scripts \
+      $path_ldsc \
+      $report
+    fi
   fi
 done
-
-
-# TODO: need a new general script
-# TODO: collect_concatenate_gwas_chromosomes.sh
