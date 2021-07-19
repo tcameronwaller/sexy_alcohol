c30987e7d29246dca240caec42f1d10cfba9d5e8
T. Cameron Waller <tcameronwaller@gmail.com>
2021-07-19 15:26:22 -0500

    new pipe script structure

--- scripts/record/2021-07-21/6_collect_submit_batch_jobs.sh
+++ scripts/record/2021-07-21/6_collect_submit_batch_jobs.sh
@@ -12,14 +12,12 @@
 # Organize paths.
 # Read private, local file paths.
 cd ~/paths
-path_ldsc=$(<"./tools_ldsc.txt")
 path_process=$(<"./process_sexy_alcohol.txt")
-
 path_dock="$path_process/dock"
-path_gwas_container="${path_dock}/gwas_complete/cohorts_models" # selection
+path_gwas_source_container="${path_dock}/gwas_complete/cohorts_models" # selection
 
 path_scripts_record="$path_process/sexy_alcohol/scripts/record/2021-07-21"
-path_promiscuity_scripts="${path_process}/promiscuity/scripts"
+path_batch_instances="${path_gwas_source_container}/post_process_batch_instances.txt"
 
 ###########################################################################
 # Define explicit inclusions and exclusions.
@@ -36,7 +34,6 @@ unset IFS
 # Execute procedure.
 
 # Initialize batch instances.
-path_batch_instances="${path_gwas_container}/batch_instances_post_process.txt"
 rm $path_batch_instances
 
 # Iterate on directories for GWAS on cohorts and hormones.
@@ -44,61 +41,58 @@ rm $path_batch_instances
 #shopt -s nullglob
 pattern_gwas_chromosome_file="report.*.glm.linear" # do not expand with full path yet
 pattern_gwas_concatenation_file="gwas_concatenation.txt.gz"
-cd $path_gwas_container
+cd $path_gwas_source_container
 for path_directory in `find . -maxdepth 1 -mindepth 1 -type d -not -name .`; do
   if [ -d "$path_directory" ]; then
     # Current content item is a directory.
     # Extract directory's base name.
-    directory="$(basename -- $path_directory)"
+    study="$(basename -- $path_directory)"
     #echo $directory
     # Determine whether directory contains valid GWAS summary statistics
     # across chromosomes.
     # Check for chromosome 22, assuming that all chromosomes completed
     # sequentially.
-    matches_chromosome=$(find "${path_gwas_container}/${directory}/chromosome_22" -name "$pattern_gwas_chromosome_file")
+    matches_chromosome=$(find "${path_gwas_source_container}/${study}/chromosome_22" -name "$pattern_gwas_chromosome_file")
     match_chromosome_file=${matches_chromosome[0]}
     if [[ -n $matches_chromosome && -f $match_chromosome_file ]]; then
       echo "----------"
-      echo "Found chromosome-22 sum stats for: ${directory}"
+      echo "----------"
+      echo "----------"
+      echo "Found chromosome-22 sum stats for: ${study}"
       echo $matches_chromosome
       echo "Found match file: ${match_chromosome_file}"
       # Determine whether directory already contains a concatenation file.
-      matches_concatenation=$(find "${path_gwas_container}/${directory}" -name "$pattern_gwas_concatenation_file")
+      matches_concatenation=$(find "${path_gwas_source_container}/${study}" -name "$pattern_gwas_concatenation_file")
       match_concatenation_file=${matches_concatenation[0]}
       if [[ -n $matches_concatenation && -f $match_concatenation_file ]]; then
         echo "-----"
-        echo "Found GWAS concatenation for: ${directory}"
+        echo "Found GWAS concatenation for: ${study}"
         echo "Found match file: ${match_concatenation_file}"
-        #echo $directory >> $path_batch_instances
+      else
+        echo "Study has chromosome GWAS summary statistics but no concatenation."
+        echo $study >> $path_batch_instances
       fi
     fi
   fi
 done
 
-if false; then
-  # Read batch instances.
-  readarray -t batch_instances < $path_batch_instances
-  batch_instances_count=${#batch_instances[@]}
-  echo "----------"
-  echo "count of batch instances: " $batch_instances_count
-  echo "first batch instance: " ${batch_instances[0]} # notice base-zero indexing
-  echo "last batch instance: " ${batch_instances[batch_instances_count - 1]}
-fi
+# Read batch instances.
+readarray -t batch_instances < $path_batch_instances
+batch_instances_count=${#batch_instances[@]}
+echo "----------"
+echo "count of batch instances: " $batch_instances_count
+echo "first batch instance: " ${batch_instances[0]} # notice base-zero indexing
+echo "last batch instance: " ${batch_instances[batch_instances_count - 1]}
 
 # Execute batch with grid scheduler.
 if false; then
-  report="true" # "true" or "false"
   # Submit array batch to Sun Grid Engine.
   # Array batch indices must start at one (not zero).
   qsub -t 1-${batch_instances_count}:1 -o \
-  "${path_gwas_parent}/out.txt" -e "${path_gwas_parent}/error.txt" \
-  "${path_promiscuity_scripts}/run_plink_gwas_collection_format_munge_heritability.sh" \
+  "${path_gwas_source_container}/post_process_out.txt" -e "${path_gwas_source_container}/post_process_error.txt" \
+  "${path_scripts_record}/7_run_batch_jobs_gwas_concatenation_format_munge_heritability.sh" \
   $path_batch_instances \
   $batch_instances_count \
-  $path_gwas_parent \
-  $path_heritability_parent \
-  $path_genetic_reference \
-  $path_promiscuity_scripts \
-  $path_ldsc \
-  $report
+  $path_gwas_source_container \
+  $path_scripts_record
 fi
