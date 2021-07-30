d659a70479a8966d382c74b1b28438ea13e4f249
T. Cameron Waller <tcameronwaller@gmail.com>
2021-07-30 12:54:42 -0500

    switch to organization procedure

--- package/description.py
+++ package/description.py
@@ -329,6 +329,7 @@ def execute_procedure(
     pail_summary = (
         ukb_organization.execute_describe_cohorts_models_phenotypes(
             table=source["table_phenotypes"],
+            genotype_cohorts=False, # genotype cohorts are slow
             set="sex_hormones",
             path_dock=path_dock,
             report=True,
