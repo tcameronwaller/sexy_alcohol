4ce4695b68fdc444a9f1fc0760abda0ed6ff61ee
T. Cameron Waller <tcameronwaller@gmail.com>
2021-08-19 13:04:49 -0500

    ready for regression in alcohol cohorts

--- package/regression.py
+++ package/regression.py
@@ -126,6 +126,101 @@ def read_source(
 
 
 
+# TODO: filter to current alcohol consumers
+# TODO: then alcohol_frenquency as ordinal outcome
+
+
+
+def organize_cohorts_models_phenotypes_regressions_alcohol(
+    table=None,
+    report=None,
+):
+    """
+    Organize regressions.
+
+    arguments:
+        table (object): Pandas data frame of phenotype variables across UK
+            Biobank cohort
+        report (bool): whether to print reports
+
+    raises:
+
+    returns:
+        (dict): information from regressions
+
+    """
+
+    # Define relevant cohorts.
+    cohorts_records = ukb_strat.stratify_set_alcohol_sex_menopause_age(
+        table=table
+    )
+
+    # TODO: TCW 19 August 2021
+    # TODO: eventually... I will need cohort-specific models...
+
+    # Define outcome dependent variables.
+    outcomes = [
+        "alcohol_frequency",
+    ]
+    # Define predictor independent variables.
+    predictors_site_components = [
+        "site-component_1", "site-component_2", "site-component_3",
+        "site-component_4", "site-component_5", "site-component_6",
+        "site-component_7", "site-component_8", "site-component_9",
+        "site-component_10", "site-component_11", "site-component_12",
+        "site-component_13", "site-component_14", "site-component_15",
+        "site-component_16", "site-component_17", "site-component_18",
+        "site-component_19", "site-component_20",
+    ]
+    predictors_month_components = [
+        "month-component_1", "month-component_2", "month-component_3",
+        "month-component_4", "month-component_5", "month-component_6",
+        "month-component_7", "month-component_8", "month-component_9",
+        "month-component_10",
+    ]
+    predictors_month_indicators = [
+        "month-indicator_January", "month-indicator_February",
+        "month-indicator_March", #"month-indicator_April",
+        "month-indicator_May", "month-indicator_June",
+        "month-indicator_July", "month-indicator_August",
+        "month-indicator_September", "month-indicator_October",
+        "month-indicator_November", "month-indicator_December",
+    ]
+
+    # Iterate across cohorts.
+    for cohort_record in cohorts_records:
+        cohort = cohort_record["cohort"]
+        menstruation = cohort_record["menstruation"]
+        table_cohort = cohort_record["table"]
+        # Iterate across outcomes (dependent variables).
+        for outcome in outcomes:
+            # Specify predictors.
+            #predictors = predictors_month_components
+            predictors = predictors_month_components
+
+            # Report.
+            if report:
+                utility.print_terminal_partition(level=3)
+                print("report: ")
+                print("organize_cohorts_models_phenotypes_regressions()")
+                utility.print_terminal_partition(level=5)
+                print("cohort: " + str(cohort))
+                print("outcome: " + str(outcome))
+                utility.print_terminal_partition(level=5)
+            pail_regression = regression.regress_linear_ordinary_least_squares(
+                dependence=outcome, # parameter
+                independence=predictors, # parameter
+                threshold_samples=100,
+                table=table_cohort,
+                report=report,
+            )
+            pass
+        pass
+
+    # Compile information.
+    pail = dict()
+    # Return information.
+    return pail
 
 
 def organize_cohorts_models_phenotypes_regressions(
@@ -273,7 +368,7 @@ def execute_procedure(
     )
 
     # Preliminary organization of regressions.
-    pail = organize_cohorts_models_phenotypes_regressions(
+    pail = organize_cohorts_models_phenotypes_regressions_alcohol(
         table=source["table_phenotypes"],
         report=True
     )
