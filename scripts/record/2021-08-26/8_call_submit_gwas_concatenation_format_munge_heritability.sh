8588c5c2ff19a8985014720da79d9b8e70197f2e
T. Cameron Waller <tcameronwaller@gmail.com>
2021-09-08 21:32:03 -0500

    ready for z-score version of linear measurement unadjust

--- scripts/record/2021-08-26/8_call_submit_gwas_concatenation_format_munge_heritability.sh
+++ scripts/record/2021-08-26/8_call_submit_gwas_concatenation_format_munge_heritability.sh
@@ -16,8 +16,8 @@
 #cohorts_models="cohorts_models_linear_imputation"           # 72 GWAS started at 19:37 on 7 September 2021
 #cohorts_models="cohorts_models_linear_imputation_unadjust"  # 72 GWAS started at 19:39 on 7 September 2021
 
-cohorts_models="cohorts_models_linear_measurement"          # z-score coefficients 72 GWAS started at ___ on 8 September 2021
-#cohorts_models="cohorts_models_linear_measurement_unadjust" # z-score coefficients 72 GWAS started at ___ on 8 September 2021
+#cohorts_models="cohorts_models_linear_measurement"          # z-score coefficients 72 GWAS started at 21:31 on 8 September 2021
+cohorts_models="cohorts_models_linear_measurement_unadjust" # z-score coefficients 72 GWAS started at ___ on 8 September 2021
 #cohorts_models="cohorts_models_linear_imputation"           # z-score coefficients 72 GWAS started at ___ on 8 September 2021
 #cohorts_models="cohorts_models_linear_imputation_unadjust"  # z-score coefficients 72 GWAS started at ___ on 8 September 2021
 
