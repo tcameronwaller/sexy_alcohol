b3eda87445e0334b2a55fe8a89e3786d66a0ca31
T. Cameron Waller <tcameronwaller@gmail.com>
2021-11-04 12:07:37 -0500

    file name

--- scripts/record/2021-11-04/13_filter_allele_frequency_threshold.sh
+++ scripts/record/2021-11-04/13_filter_allele_frequency_threshold.sh
@@ -22,7 +22,7 @@ path_dock="$path_process/dock"
 path_allele_frequency="${path_dock}/allele_frequency"
 
 path_source_report="${path_allele_frequency}/allele_frequency_concatenation.afreq.gz"
-path_target_report="${path_allele_frequency}/allele_frequency_filter_0_1.afreq"
+path_target_report="${path_allele_frequency}/allele_frequency_filter_0_05.afreq"
 path_target_report_compress="${path_allele_frequency}/allele_frequency_filter_0_05.afreq.gz"
 
 ################################################################################
