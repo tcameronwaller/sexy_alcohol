2a472227a784243c3f71b37f0f27f7ae2e31a2a5
T. Cameron Waller <tcameronwaller@gmail.com>
2021-12-01 16:23:22 -0600

    next

--- scripts/record/2021-12-02/gwas_concatenation/1-2_call_submit_gwas_concatenation.sh
+++ scripts/record/2021-12-02/gwas_concatenation/1-2_call_submit_gwas_concatenation.sh
@@ -14,8 +14,8 @@
 #cohorts_models="cohorts_models_linear_measurement"          # 36 GWAS; TCW started at 16:14 on 1 December 2021
 # wait for GWAS to complete... cohorts_models="cohorts_models_linear_measurement_unadjust" # 36 GWAS; TCW started at ___ on 1 December 2021
 
-cohorts_models="cohorts_models_linear_imputation"           # 36 GWAS; TCW started at ___ on 1 December 2021
-#cohorts_models="cohorts_models_linear_imputation_unadjust"  # 36 GWAS; TCW started at ___ on 1 December 2021
+#cohorts_models="cohorts_models_linear_imputation"           # 36 GWAS; TCW started at 16:22 on 1 December 2021
+cohorts_models="cohorts_models_linear_imputation_unadjust"  # 36 GWAS; TCW started at ___ on 1 December 2021
 
 pattern_gwas_report_file="report.*.glm.linear" # do not expand with full path yet
 chromosome_x="false" # whether to collect GWAS summary statistics report for Chromosome X
