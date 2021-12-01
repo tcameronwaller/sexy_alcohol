3608f1f5f735a4fc5f301815bc444c907782da09
T. Cameron Waller <tcameronwaller@gmail.com>
2021-12-01 15:58:58 -0600

    ready for concatenation on imputed measurements with adjustment

--- scripts/record/2021-12-02/gwas_concatenation/1-1_call_submit_gwas_concatenation.sh
+++ scripts/record/2021-12-02/gwas_concatenation/1-1_call_submit_gwas_concatenation.sh
@@ -12,10 +12,10 @@
 # General parameters.
 
 
-cohorts_models="cohorts_models_linear_measurement"          # 36 GWAS; TCW started at ___ on 1 December 2021
+#cohorts_models="cohorts_models_linear_measurement"          # 36 GWAS; TCW started at 15:58 on 1 December 2021
 # wait for GWAS to complete... cohorts_models="cohorts_models_linear_measurement_unadjust" # 36 GWAS; TCW started at ___ on 1 December 2021
 
-#cohorts_models="cohorts_models_linear_imputation"           # 36 GWAS; TCW started at ___ on 1 December 2021
+cohorts_models="cohorts_models_linear_imputation"           # 36 GWAS; TCW started at ___ on 1 December 2021
 #cohorts_models="cohorts_models_linear_imputation_unadjust"  # 36 GWAS; TCW started at ___ on 1 December 2021
 
 # wait for GWAS... cohorts_models="cohorts_models_logistic_detection"          # 40 GWAS; TCW started at ___ on 1 December 2021
