f97ba1119dd56d3461d558e7962770529e1e6c47
T. Cameron Waller <tcameronwaller@gmail.com>
2022-07-13 15:18:56 -0500

    typo error

--- scripts/record/2022-08-01/gwas_concatenation/1_submit_batch_gwas_concatenation.sh
+++ scripts/record/2022-08-01/gwas_concatenation/1_submit_batch_gwas_concatenation.sh
@@ -59,7 +59,7 @@ rm $path_file_batch_instances
 instances_sets=()
 instances_sets+=("oestradiol_logistic;logistic;true")              # 18 GWAS
 instances_sets+=("oestradiol_linear;linear;true")                  # 30 GWAS
-nstances_sets+=("oestradiol_bioavailable_linear;linear;true")     # 18 GWAS
+instances_sets+=("oestradiol_bioavailable_linear;linear;true")     # 18 GWAS
 instances_sets+=("oestradiol_free_linear;linear;true")             # 18 GWAS
 instances_sets+=("testosterone_logistic;logistic;true")            # 18 GWAS
 instances_sets+=("testosterone_linear;linear;true")                # 54 GWAS
