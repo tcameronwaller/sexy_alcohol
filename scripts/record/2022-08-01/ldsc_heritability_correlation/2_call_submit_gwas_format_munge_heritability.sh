1724470414cea4dc298ed73bac6a0e58929836c9
T. Cameron Waller <tcameronwaller@gmail.com>
2022-07-14 12:31:35 -0500

    ready to submit batch for 'testosterone_logistic'

--- scripts/record/2022-08-01/ldsc_heritability_correlation/2_call_submit_gwas_format_munge_heritability.sh
+++ scripts/record/2022-08-01/ldsc_heritability_correlation/2_call_submit_gwas_format_munge_heritability.sh
@@ -19,11 +19,11 @@
 ################################################################################
 # General parameters.
 
-cohorts_models="oestradiol_logistic"                   # 18 GWAS
+#cohorts_models="oestradiol_logistic"                   # 18 GWAS; started on 14 July 2022
 #cohorts_models="oestradiol_linear"                     # 30 GWAS
 #cohorts_models="oestradiol_bioavailable_linear"        # 18 GWAS
 #cohorts_models="oestradiol_free_linear"                # 18 GWAS
-#cohorts_models="testosterone_logistic"                 # 18 GWAS
+cohorts_models="testosterone_logistic"                 # 18 GWAS; 
 #cohorts_models="testosterone_linear"                   # 54 GWAS
 #cohorts_models="testosterone_bioavailable_linear"      # 18 GWAS
 #cohorts_models="testosterone_free_linear"              # 18 GWAS
