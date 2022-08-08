72b0fe2054a3f40a7916faf9b1f619a2436a9185
T. Cameron Waller <tcameronwaller@gmail.com>
2022-08-08 13:39:56 -0500

    ready for assembly procedure

--- scripts/execute_sexy_alcohol.sh
+++ scripts/execute_sexy_alcohol.sh
@@ -42,11 +42,11 @@ fi
 #python3 $path_package/interface.py main --path_dock $path_dock --scratch
 
 # Routine: uk_biobank
-#python3 $path_package/interface.py uk_biobank --path_dock $path_dock --assembly # TCW; 30 March 2022
-#python3 $path_package/interface.py uk_biobank --path_dock $path_dock --importation # TCW; 31 March 2022
-#python3 $path_package/interface.py uk_biobank --path_dock $path_dock --organization # TCW; 07 June 2022
+python3 $path_package/interface.py uk_biobank --path_dock $path_dock --assembly # TCW; 08 August 2022
+#python3 $path_package/interface.py uk_biobank --path_dock $path_dock --importation # TCW; ___ 2022
+#python3 $path_package/interface.py uk_biobank --path_dock $path_dock --organization # TCW; ___ 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --stratification # TCW; 09 April 2022, 11 April 2022
-python3 $path_package/interface.py uk_biobank --path_dock $path_dock --description # TCW; 02 August 2022
+#python3 $path_package/interface.py uk_biobank --path_dock $path_dock --description # TCW; 02 August 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --regression # TCW; 20 April 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --collection # TCW; 01 August 2022
 
