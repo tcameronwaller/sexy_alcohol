f18689482dc4ab11231fc778781ed194c0a0a381
T. Cameron Waller <tcameronwaller@gmail.com>
2022-09-20 13:49:46 -0500

    procedure switch

--- scripts/execute_sexy_alcohol.sh
+++ scripts/execute_sexy_alcohol.sh
@@ -46,7 +46,7 @@ fi
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --importation # TCW; 08 August 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --organization # TCW; 08 August 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --stratification # TCW; ___ 2022
-#python3 $path_package/interface.py uk_biobank --path_dock $path_dock --description # TCW; ___ 2022
+python3 $path_package/interface.py uk_biobank --path_dock $path_dock --description # TCW; 20 September 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --regression # TCW; 09 August 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --collection # TCW; ___ 2022
 
@@ -55,7 +55,7 @@ fi
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_organization # TCW; 20 June 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_regression # TCW; 20 June 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mcita_assembly # TCW; 24 August 2022
-python3 $path_package/interface.py stragglers --path_dock $path_dock --mcita_organization # TCW; 24 August 2022
+#python3 $path_package/interface.py stragglers --path_dock $path_dock --mcita_organization # TCW; 24 August 2022
 
 ################################################################################
 # Deactivate Python Virtual Environment.
