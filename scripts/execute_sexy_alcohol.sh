44ac781eb6e4025b7b19888af68e3cc306d2815a
T. Cameron Waller <tcameronwaller@gmail.com>
2022-10-13 14:16:24 -0500

    switch execution procedure

--- scripts/execute_sexy_alcohol.sh
+++ scripts/execute_sexy_alcohol.sh
@@ -46,8 +46,9 @@ fi
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --importation # TCW; 08 August 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --organization # TCW; 08 August 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --stratification # TCW; ___ 2022
+python3 $path_package/interface.py uk_biobank --path_dock $path_dock --genotype # TCW; 13 October 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --description # TCW; 20 September 2022
-python3 $path_package/interface.py uk_biobank --path_dock $path_dock --plot # TCW; 6 October 2022
+#python3 $path_package/interface.py uk_biobank --path_dock $path_dock --plot # TCW; 6 October 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --regression # TCW; 09 August 2022
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --collection # TCW; ___ 2022
 
